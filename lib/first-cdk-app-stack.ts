a93d91c4759d8aa1b674408df2e791c7a0662853
Venugopalkr29 <venugopal@geometrysystems.io>
2025-07-28 00:12:48 +0530

    feat: added s3 bucket name and key

--- lib/first-cdk-app-stack.ts
+++ lib/first-cdk-app-stack.ts
@@ -1,12 +1,30 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { LambdaApiFunction } from '../src/api/rest/lambda';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class FirstCdkAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new LambdaApiFunction(this, 'create-basic-lambda', {});
+    const bucketName = new cdk.CfnParameter(this, 'bucketName', {
+      type: 'String',
+      description: 'The name of the S3 bucket to create',
+    });
+
+    const bucketKey = new cdk.CfnParameter(this, 'bucketKey', {
+      type: 'String',
+      description: 'The key for the object in the S3 bucket',
+    });
+
+    const bucket = new cdk.aws_s3.Bucket(this, 'MyBucket', {
+      bucketName: bucketName.valueAsString,
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Only for dev/test environments
+    });
+
+    new LambdaApiFunction(this, 'create-basic-lambda', {
+      code: lambda.Code.fromBucket(bucket, bucketKey.valueAsString),
+    });
   }
 }
